b866282d4045eb5ab3b370ec2dd31670e13eea83
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-10-08 20:30:45 +0800

    using helper function to get size

--- my_lecture_code/lec5_lists2/SLList.java
+++ my_lecture_code/lec5_lists2/SLList.java
@@ -28,10 +28,38 @@ public class SLList {
         return first.item;
     }
 
+    /** adds an item to the end of last */
+    public void addLast(int x){
+        IntNode p = first;
+        while (p.next != null) {    // scan to the end
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+    }
+
+    /** recursize helper function */
+    private static int size(IntNode p){
+        if(p.next == null) return 1;
+        return 1 + size(p.next);
+    }
+
+    public int size(){
+        // int sz = 0;
+        // IntNode p = first;
+        // while (p != null) {
+        //     p = p.next;
+        //     sz += 1;
+        // }
+        // return sz;
+        return size(first);
+    }
+
     public static void main(String[] args) {
         SLList L = new SLList(15);
         L.addFirst(10);
         L.addFirst(5);
+        L.addLast(20);
         System.out.println(L.getFirst());
+        System.out.println(L.size());
     }
 }
