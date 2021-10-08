638e4c5a6f7e730e5796c077b615a54ca7f3d8e1
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-10-08 20:52:36 +0800

    using sentinel node

--- my_lecture_code/lec5_lists2/SLList.java
+++ my_lecture_code/lec5_lists2/SLList.java
@@ -2,7 +2,7 @@ package my_lecture_code.lec5_lists2;
 
 /** An SLList is a list of integers, contains IntNode */
 public class SLList {
-    private IntNode first;  // 防止被访问修改
+    private IntNode sentinel;   // 哨兵节点，一直存在
     private int size;
 
     // IntNode 不会使用外部类， 可以static， 节省一点内存
@@ -18,35 +18,31 @@ public class SLList {
 
     /** create an empty list */
     public SLList(){
-        first = null;
+        sentinel = new IntNode(2333, null); // data 无用
         size = 0;
     }
 
     public SLList(int x){
-        first = new IntNode(x, null);
+        sentinel = new IntNode(2333, null);
+        sentinel.next = new IntNode(x, null);
         size = 1;
     }
 
     /** adds x to front of List */
     public void addFirst(int x){
-        first = new IntNode(x, first);
         size += 1;
+        sentinel.next = new IntNode(x, sentinel.next);
     }
 
     public int getFirst(){
-        return first.item;
+        return sentinel.next.item;
     }
 
     /** adds an item to the end of last */
     public void addLast(int x){
         size += 1;
 
-        if(first == null){
-            first = new IntNode(x, null);
-            return;
-        }
-        
-        IntNode p = first;
+        IntNode p = sentinel;
         while (p.next != null) {    // scan to the end
             p = p.next;
         }
@@ -55,8 +51,8 @@ public class SLList {
 
     public static void main(String[] args) {
         SLList L = new SLList();
-        // L.addFirst(10);
-        // L.addFirst(5);
+        L.addFirst(10);
+        L.addFirst(5);
         L.addLast(20);
         System.out.println(L.getFirst());
         System.out.println(L.size);
