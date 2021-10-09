8ce989094217933318bd52c21ee40bd1d270d5ba
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-10-09 11:07:46 +0800

    begin to write AList.java

--- lec7_lists4/AList.java
+++ lec7_lists4/AList.java
@@ -20,34 +20,22 @@ public class AList<Item> {
 
     /** Creates an empty list. */
     public AList() {
-        items = (Item[]) new Object[100];
-        size = 0;
     }
 
     /** Resizes the underlying array to the target capacity. */
     private void resize(int capacity) {
-        Item[] a = (Item[]) new Object[capacity];
-        System.arraycopy(items, 0, a, 0, size);
-        items = a;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
-        if (size == items.length) {
-            resize(size + 1);
-        }
-
-        items[size] = x;
-        size = size + 1;
     }
 
     /** Returns the item from the back of the list. */
-    public Item getLast() {
-        return items[size - 1];
+    public Item getLast(){
+
     }
     /** Gets the ith item in the list (0 is the front). */
     public Item get(int i) {
-        return items[i];
     }
 
     /** Returns the number of items in the list. */
@@ -58,9 +46,6 @@ public class AList<Item> {
     /** Deletes item from back of the list and
       * returns deleted item. */
     public Item removeLast() {
-        Item x = getLast();
-        items[size - 1] = null;
-        size = size - 1;
-        return x;
+
     }
 } 
