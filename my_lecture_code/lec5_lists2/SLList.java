46fb32bf26e5fd49897d2d85e2bbdb4a2637d93a
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-10-09 09:33:37 +0800

    using ADT, any type of SLList

--- my_lecture_code/lec5_lists2/SLList.java
+++ my_lecture_code/lec5_lists2/SLList.java
@@ -1,15 +1,15 @@
 
 /** An SLList is a list of integers, contains IntNode */
-public class SLList {
-    private IntNode sentinel;   // 哨兵节点，一直存在
+public class SLList<Stuff> {
+    private StuffNode sentinel;   // 哨兵节点，一直存在
     private int size;
 
     // IntNode 不会使用外部类， 可以static， 节省一点内存
-    private static class IntNode {  // nested class 嵌套类
-        public int item;
-        public IntNode next;
+    public class StuffNode {  // nested class 嵌套类
+        public Stuff item;
+        public StuffNode next;
     
-        public IntNode(int i, IntNode n) {
+        public StuffNode(Stuff i, StuffNode n) {
             item = i;
             next = n;
         }
@@ -17,39 +17,39 @@ public class SLList {
 
     /** create an empty list */
     public SLList(){
-        sentinel = new IntNode(2333, null); // data 无用
+        sentinel = new StuffNode(null, null); // data 无用
         size = 0;
     }
 
-    public SLList(int x){
-        sentinel = new IntNode(2333, null);
-        sentinel.next = new IntNode(x, null);
+    public SLList(Stuff x){
+        sentinel = new StuffNode(null, null);
+        sentinel.next = new StuffNode(x, null);
         size = 1;
     }
 
     /** adds x to front of List */
-    public void addFirst(int x){
+    public void addFirst(Stuff x){
         size += 1;
-        sentinel.next = new IntNode(x, sentinel.next);
+        sentinel.next = new StuffNode(x, sentinel.next);
     }
 
-    public int getFirst(){
+    public Stuff getFirst(){
         return sentinel.next.item;
     }
 
     /** adds an item to the end of last */
-    public void addLast(int x){
+    public void addLast(Stuff x){
         size += 1;
 
-        IntNode p = sentinel;
+        StuffNode p = sentinel;
         while (p.next != null) {    // scan to the end
             p = p.next;
         }
-        p.next = new IntNode(x, null);
+        p.next = new StuffNode(x, null);
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList();
+        SLList<Integer> L = new SLList<>(15);
         L.addFirst(10);
         L.addFirst(5);
         L.addLast(20);
